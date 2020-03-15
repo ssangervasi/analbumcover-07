e16d54892a263f0e44fb6b1131662fa26d919178
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 16:04:36 -0700

    add temp eslintignore to avoid linting non-tests

--- src/analbumcover.test.ts
+++ src/analbumcover.test.ts
@@ -2,19 +2,18 @@ import * as Spelling from './spelling'
 import { rephrase } from './analbumcover'
 
 describe('rephrase', () => {
-	let nodehun : Spelling.Nodehun, spelling : Spelling.Spelling
+	let spelling : Spelling.Spelling
 
 	beforeAll(async () => {
-		nodehun = await Spelling.initNodehun()
-	})
-
-	beforeEach(() => {
+		const nodehun : Spelling.Nodehun = await Spelling.initNodehun()
 		spelling = new Spelling.NodehunSpelling(nodehun)
 	})
 
-	describe('with Spelling isCorrect=true', () => {
-		test('it should do a thing', () => {
-			expect(rephrase('do a thing', spelling)).toBe('doathing')
-		})
+	test('it should do a thing', () => {
+		expect(rephrase('do a thing', spelling)).toBe('doathing')
+	})
+
+	test('it should do a thing', () => {
+		expect(rephrase('do a thing', spelling)).toBe('doathing')
 	})
 })
