41231de712b3a8f3f537c2185baad448bcd75520
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 16:43:32 -0700

    hotdog or not hotdog

--- src/testconfig.ts
+++ src/testconfig.ts
@@ -8,6 +8,9 @@ const WORD_LIST : Array<TestConfig> = [
 	{ word: 'tea', expected: {1: 'tea', 2: 'tea', 3: 'tea'} },
 	{ word: 'milk tea', expected: {1: 'milktea', 2: null, 3: null} },
 	{ word: 'an album cover', expected: {1: 'analbumcover', 2: null, 3: null} },
+	{ word: 'hotdog', expected: {1: 'hotdog', 2: null, 3: 'hotdog'} },
+	{ word: 'hot dog', expected: {1: 'hotdog', 2: null, 3: 'hotdog'} },
+	{ word: 'not hotdog', expected: {1: 'nothotdog', 2: null, 3: 'nothotdog'} },
 ]
 
 export { TestConfig, WORD_LIST }
