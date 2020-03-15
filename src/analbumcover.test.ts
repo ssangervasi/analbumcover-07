d8b339fc758e7bd3e7f12505d7bfc0df1c0a9332
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 16:14:39 -0700

    add more tests

--- src/analbumcover.test.ts
+++ src/analbumcover.test.ts
@@ -9,11 +9,47 @@ describe('rephrase', () => {
 		spelling = new Spelling.NodehunSpelling(nodehun)
 	})
 
-	test('it should do a thing', () => {
-		expect(rephrase('do a thing', spelling)).toBe('doathing')
+	// const TEST_WORDS = Object.freeze(['', 'an elegant milk tea', 'an album cover', 'hotdog', 'not hotdog']);
+
+	describe('min word length = 1', () => {
+		test('it should have zenlike empty spaces', () => {
+			expect(rephrase('', spelling, 1)).toBe(null)
+		})
+
+		test('it should do a thing', () => {
+			expect(rephrase('do a thing', spelling, 1)).toBe('doathing')
+		})
+
+		test('it should have album covers', () => {
+			expect(rephrase('an album cover', spelling, 1)).toBe('analbumcover')
+		})
 	})
 
-	test('it should do a thing', () => {
-		expect(rephrase('do a thing', spelling)).toBe('doathing')
+	describe('min word length = 2', () => {
+		test('it should have zenlike empty spaces', () => {
+			expect(rephrase('', spelling, 2)).toBe(null)
+		})
+
+		test('it should do a thing', () => {
+			expect(rephrase('do a thing', spelling, 2)).toBe(null)
+		})
+
+		test('it should have album covers', () => {
+			expect(rephrase('an album cover', spelling, 2)).toBe(null)
+		})
+	})
+
+	describe('min word length = 3', () => {
+		test('it should have zenlike empty spaces', () => {
+			expect(rephrase('', spelling, 3)).toBe(null)
+		})
+
+		test('it should do a thing', () => {
+			expect(rephrase('do a thing', spelling, 3)).toBe(null)
+		})
+
+		test('it should have album covers', () => {
+			expect(rephrase('an album cover', spelling, 3)).toBe(null)
+		})
 	})
 })
