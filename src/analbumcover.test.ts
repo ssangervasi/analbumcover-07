f59f7d6e4f3db086d500766ea88b47c6f9d6d1c6
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 15:57:39 -0700

    scope commands to installed version (revert if needed (sorry))

--- src/analbumcover.test.ts
+++ src/analbumcover.test.ts
@@ -1,7 +1,20 @@
-import {
-	rephrase,
-} from './analbumcover'
-
+import * as Spelling from './spelling';
+import { rephrase } from './analbumcover'
 
 describe('rephrase', () => {
+  let nodehun : Spelling.Nodehun, spelling : Spelling.Spelling;
+
+  beforeAll(async () => {
+    nodehun = await Spelling.initNodehun();
+  });
+
+  beforeEach(() => {
+    spelling = new Spelling.NodehunSpelling(nodehun);
+  });
+
+  describe('with Spelling isCorrect=true', () => {
+    test('it should do a thing', () => {
+      expect(rephrase('do a thing', spelling)).toBe('rabbit');
+    });
+  });
 })
