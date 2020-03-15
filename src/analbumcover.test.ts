30bf48c23989534427e5d3d96b6e80fed0190351
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 16:00:51 -0700

    add a test. of sorts.

--- src/analbumcover.test.ts
+++ src/analbumcover.test.ts
@@ -1,20 +1,20 @@
-import * as Spelling from './spelling';
+import * as Spelling from './spelling'
 import { rephrase } from './analbumcover'
 
 describe('rephrase', () => {
-  let nodehun : Spelling.Nodehun, spelling : Spelling.Spelling;
+	let nodehun : Spelling.Nodehun, spelling : Spelling.Spelling
 
-  beforeAll(async () => {
-    nodehun = await Spelling.initNodehun();
-  });
+	beforeAll(async () => {
+		nodehun = await Spelling.initNodehun()
+	})
 
-  beforeEach(() => {
-    spelling = new Spelling.NodehunSpelling(nodehun);
-  });
+	beforeEach(() => {
+		spelling = new Spelling.NodehunSpelling(nodehun)
+	})
 
-  describe('with Spelling isCorrect=true', () => {
-    test('it should do a thing', () => {
-      expect(rephrase('do a thing', spelling)).toBe('rabbit');
-    });
-  });
+	describe('with Spelling isCorrect=true', () => {
+		test('it should do a thing', () => {
+			expect(rephrase('do a thing', spelling)).toBe('doathing')
+		})
+	})
 })
