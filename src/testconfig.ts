5d8a41bae8e324cf85cc93e1ea31f06dae4388d0
🐿️ <1071472+clanam@users.noreply.github.com>
2020-03-15 17:09:25 -0700

    update glyphs

--- src/testconfig.ts
+++ src/testconfig.ts
@@ -4,22 +4,26 @@ interface TestConfig {
   word : string,
 }
 
+const ABCD = String.fromCharCode(55357, 56612)
 const DISC = String.fromCharCode(55357, 56511)
+const DOG_ = String.fromCharCode(55357, 56374)
+const GREK = String.fromCharCode(55356, 56812, 55356, 56823)
 const HDOG = String.fromCharCode(55356, 57133)
+const NDOG = String.fromCharCode(55357, 57003, 55356, 57133)
 const NOPE = String.fromCharCode(55356, 56882)
 const TEA_ = String.fromCharCode(55356, 57205)
 
 const WORD_LIST : Array<TestConfig> = [
 	{ glyph: NOPE, word: '', expected: {1: null, 2: null, 3: null} },
-	{ glyph: NOPE, word: 'μ', expected: {1: null, 2: null, 3: null} },
-	{ glyph: NOPE, word: '子犬', expected:{1: null, 2: null, 3: null} },
-	{ glyph: NOPE, word: 'asdfgh', expected: {1: 'asdfgh', 2: null, 3: null} },
+	{ glyph: GREK, word: 'μ', expected: {1: null, 2: null, 3: null} },
+	{ glyph: DOG_, word: '子犬', expected:{1: null, 2: null, 3: null} },
+	{ glyph: ABCD, word: 'asdfgh', expected: {1: 'asdfgh', 2: null, 3: null} },
 	{ glyph: TEA_, word: 'tea', expected: {1: 'tea', 2: 'tea', 3: 'tea'} },
 	{ glyph: TEA_, word: 'milktea', expected: {1: 'milktea', 2: null, 3: null} },
 	{ glyph: DISC, word: 'analbumcover', expected: {1: 'analbumcover', 2: null, 3: null} },
 	{ glyph: HDOG, word: 'hot dog', expected: {1: 'hotdog', 2: null, 3: 'hotdog'} },
 	{ glyph: HDOG, word: 'hotdog', expected: {1: 'hotdog', 2: null, 3: 'hotdog'} },
-	{ glyph: HDOG, word: 'not hotdog', expected: {1: 'nothotdog', 2: null, 3: 'nothotdog'} },
+	{ glyph: NDOG, word: 'not hotdog', expected: {1: 'nothotdog', 2: null, 3: 'nothotdog'} },
 ]
 
 export { TestConfig, WORD_LIST }
